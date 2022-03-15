dc8096f2c3e96e013929cd8fbba427cea6ff9656
mklr-nl <info@mklr.nl>
2022-03-15 18:59:30 +0100

    wip

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,21 @@
+import Head from "next/head";
 import { Home } from "../components/home";
 import "@fontsource/poppins";
+import { Fragment } from "react";
 
 function HomePage() {
-  return <Home />;
+  return (
+    <Fragment>
+      <Head>
+        <title>DigiHeld</title>
+        <meta
+          name="description"
+          content="Digital Product Development and Investment"
+        />
+      </Head>
+      <Home />
+    </Fragment>
+  );
 }
 
 export default HomePage;
