3837ca888b6b4bee372e31474b3af7bda91f195e
mklr-nl <info@mklr.nl>
2022-03-16 10:03:02 +0100

    wip

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,9 @@
-import Head from "next/head";
-
 import Layout from "../components/layout/layout";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
-      <Head>
-        <title>Digiheld | Digital Product Development and Investment</title>
-        <meta
-          name="description"
-          content="Digital products are programming code-based assets that deliver an interactive value proposition to the final user. ✓ Problem-solving Entrepreneurship"
-        />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
       <Component {...pageProps} />
     </Layout>
   );
--- pages/index.js
+++ pages/index.js
@@ -10,8 +10,15 @@ function HomePage() {
         <title>Digiheld | Digital Product Development and Investment</title>
         <meta
           name="description"
+          content="Digital products are programming code-based assets that deliver an interactive value proposition to the final user. ✓ Problem-solving Entrepreneurship"
+        />
+        <meta
+          property="og:title"
           content="Digital Product Development and Investment"
         />
+        <meta property="og:url" content="https://www.digiheld.nl/" />
+        <meta property="og:image" content="/public/images/digiheld-logo.png" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Home />
     </Fragment>
